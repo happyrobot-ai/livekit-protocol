2660eb3d926231449d04e175812e37453cd245c1
Carlos Álvarez Vereterra <carlos@happyrobot.ai>
2025-10-08 15:00:47 -0700

    Added participant updated

--- webhook/consts.go
+++ webhook/consts.go
@@ -28,6 +28,7 @@ const (
 	EventRoomStarted                  = "room_started"
 	EventRoomFinished                 = "room_finished"
 	EventParticipantJoined            = "participant_joined"
+	EventParticipantUpdate            = "participant_update"
 	EventParticipantLeft              = "participant_left"
 	EventParticipantConnectionAborted = "participant_connection_aborted"
 	EventTrackPublished               = "track_published"
